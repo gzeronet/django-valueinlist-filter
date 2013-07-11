e99fedadb197fe73d00c521e15e29832e2322d7b
chenyong <chenyong@ata.net.cn>
2013-07-11 10:58:28 +0800

    made pip package

--- README.rst
+++ README.rst
@@ -6,15 +6,10 @@ Queryset can be filtered by a given list on the django admin page, just like: qs
 Installation
 ------------
 
-.. code-block:: bash
-
-    cd django-valueinlist-filter
-    python setup.py install
+    [sudo] pip install django-valueinlist-filter
 
 
 Add valueinlist_filter to settings.INSTALLED_APPS:
-
-.. code-block:: python
 
     INSTALLED_APPS = (
         ...
@@ -23,8 +18,6 @@ Add valueinlist_filter to settings.INSTALLED_APPS:
 
 And then, set a CharField field in a model like this:
 
-.. code-block:: python
-
     class Model(models.Model):
         ...
         serial_number = models.CharField(max_length=32)
@@ -32,7 +25,6 @@ And then, set a CharField field in a model like this:
 
 If you want to filter the **serial_number** field by a given value list(split string by space), you can bind ValueInListFilter to this field:
 
-.. code-block:: python
 
     from valueinlist_filter.filter import ValueInListFilter
     from django.contrib import admin
@@ -45,3 +37,5 @@ If you want to filter the **serial_number** field by a given value list(split st
         )
 
 Finally, you will get a textarea form on the filter side bar, the textarea's change event will trigger form.
+
+Added a 'screenshot.png' picture.
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-valueinlist-filter',
-    version='0.1',
+    version='0.1.3',
     description='Queryset can be filtered by a given list on the django admin page, just like: qs.filter(field__in=[list])',
-    long_description=open('README.md').read(-1),
+    long_description=open('README.rst').read(-1),
     author='Chris Chen',
     author_email='gzerone@gmail.com',
     url='http://github.com/gzeronet/django-valueinlist-filter',
@@ -19,5 +19,18 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    license = 'BSD',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent'
+    ],
+    license='License :: OSI Approved :: BSD License',
 )
